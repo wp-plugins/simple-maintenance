88195622180b9d7af23c4579d7e07cff025ce0ad
naa986 <naa986@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 14:50:34 +0000

    .first commit
    git-svn-id: https://plugins.svn.wordpress.org/simple-maintenance/trunk@938556 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sm-template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width">
+    <title><?php bloginfo('name'); ?> &#8250; Simple Maintenance</title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="<?php echo SIMPLE_MAINTENANCE_URL.'/sm-style.css' ?>" type="text/css" media="all" />	
+</head>
+<body>
+    <div id="header">
+        <h2><a title="<?php bloginfo('name'); ?>" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h2>
+    </div>	
+    <div id="content">
+        <h1>Maintenance Mode</h1>
+        <p><a title="<?php bloginfo('name'); ?>" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> is currently undergoing scheduled maintenance.</p>
+        <p>Please try back again soon.</p>
+        <p>Sorry for the inconvenience.</p>			
+    </div>
+</body>
+</html>
